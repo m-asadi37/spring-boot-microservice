0e13c15308e93e4017f9e4c876cdabbca2757f62
m.asadi <masd9617.work@gmail.com>
2025-09-29 11:05:18 +0330

    api gateway service module added + security jwt token using keycloak

---
+++ api-gateway-service/src/main/java/org/masd/apigatewayservice/SecurityConfig.java
@@ -0,0 +1,26 @@
+package org.masd.apigatewayservice;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableReactiveMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) {
+        http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(exchanges ->
+                        exchanges
+                                .anyExchange().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 ->
+                        oauth2.jwt(Customizer.withDefaults()));
+        return http.build();
+    }
+}
